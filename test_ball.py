dac30e30030e5ffe3da15df78aa39669dce8642c
Rin2396 <kolkareva.dayana2016@gmail.com>
2023-10-05 20:00:34 +0300

    linter and test fixes on new branch

--- test_ball.py
+++ test_ball.py
@@ -1,8 +1,7 @@
-""""""
+"""Test to check the functionality of the function."""
 import pytest
-from typing import Tuple
-from ball import gradus_get
 
+from ball import gradus_get
 
 test_znacheniya = (
     (5, 5, 5, 5, 360.79),
@@ -13,12 +12,21 @@ test_znacheniya = (
 
 
 @pytest.mark.parametrize('radius, time, acceleration, velocity, gradus', test_znacheniya)
-def test_gradus(radius: float, time: float, acceleration: float, velocity: float, gradus: Tuple[float]):
-    """"""
-    assert gradus_get(radius, time, acceleration, velocity) == gradus
+def test_gradus(radius: float, time: float, acceleration: float, velocity: float, expected: float):
+    """Section of code to find of the degree for ball displacement.
+
+    Args:
+        radius: float ball radius.
+        time: float how much time has passed.
+        acceleration: float how fast it moves.
+        velocity: float velosity that was given.
+        expected: float degree that is supposed to be returned.
+
+    """
+    assert gradus_get(radius, time, acceleration, velocity) == expected
 
 
 @pytest.mark.xfail(raises=Exception)
 def test_exception():
-    """"""
+    """Exception test that has no args or returns."""
     assert gradus_get(0, -6, 5, 5)
